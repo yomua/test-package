4c423a8e0956019126e32063b8babb22b79153d7
daigang1228 <dg17729840156@163.com>
2021-08-04 17:48:17 +0800

    fix

--- src/notif/notif.ts
+++ src/notif/notif.ts
@@ -1,5 +1,5 @@
 import request from "../utils/request";
-export async function notif(url: string, options: any, user_id: string) {
+export async function notif(url: string, token: string, user_id: string) {
   const cond = [['user_id', '=', user_id]];
   const fields = ['time', 'title', 'state', 'data'];
   const params = [
@@ -8,10 +8,10 @@ export async function notif(url: string, options: any, user_id: string) {
     [cond, fields],
     { order: 'time desc', count: true, limit: 10, offset: 10 },
   ];
-
- 
+  
+  const options = {}
   // eslint-disable-next-line functional/immutable-data
-  options.body = JSON.stringify({
+  options.params = JSON.stringify({
     id: new Date().getTime(),
     method: 'execute',
     params,
