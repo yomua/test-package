4d1e90e9cbc827729df192b8f7e581214aed4338
hezhi <>
2021-08-04 16:13:20 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
-export * from './lib/number';
-<<<<<<< HEAD
-function a() {
-  return '111';
-}
-
-export default a;
-=======
-import { getCryptoAddress } from "./td.account";
-import { ErrorHandler } from "./index.d";
-import { notif } from "./notif/notif";
+import { getCryptoAddress } from './td.account';
+import { ErrorHandler } from './index.d';
+import { notif } from './notif/notif';
 
 function HydraxSDK(url: string, token: string, user_id: string) {
   return {
-    getCryptoAddress: (options?: any, errorHandler?: ErrorHandler) => getCryptoAddress({ url, token, user_id, options, errorHandler }),
-    getnotif: () => notif(url, token, user_id)
+    getCryptoAddress: (options?: any, errorHandler?: ErrorHandler) =>
+      getCryptoAddress({ url, token, user_id, options, errorHandler }),
+    getnotif: () => notif(url, token, user_id),
   };
 }
 
 export default HydraxSDK;
->>>>>>> b4888869e88ffcba4f3d38963634abf689e4b772
