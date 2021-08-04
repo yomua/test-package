922d737e1ed3b7cf80b06ad6e599eb1b30226aa9
yanghongwei <>
2021-08-04 15:56:55 +0800

    add td.account

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 export * from './lib/number';
 import { getCryptoAddress } from "./td.account";
-import { ErrorHandler } from "./index.d";
+import { ErrorHandler, SDKConfig } from "./index.d";
+
 
 function HydraxSDK(url: string, token: string, user_id: string) {
   return {
-    getCryptoAddress: (options?, errorHandler?: ErrorHandler) => getCryptoAddress({ url, token, user_id, options, errorHandler })
+    getCryptoAddress: (options?: SDKConfig, errorHandler?: ErrorHandler) => getCryptoAddress({ url, token, user_id, options, errorHandler })
   };
 }
 
