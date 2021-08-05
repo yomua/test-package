a4e5bf67b6eef92664a1ce9616b29660438f78f3
hezhi <>
2021-08-05 10:33:37 +0800

    udpate

--- src/td.account/index.ts
+++ src/td.account/index.ts
@@ -1,5 +1,5 @@
 import request from '../utils/request';
-
+import { AxiosRequestConfig } from 'axios';
 // options not require, should set default value in request.ts
 interface SDKConfig extends AxiosRequestConfig {
   baseUrl?: string;
