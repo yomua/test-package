c4fd0f7c634f9698cd07194fb226530d07ff5598
daigang1228 <dg17729840156@163.com>
2021-08-05 09:48:49 +0800

    fix

--- src/notif/notif.ts
+++ src/notif/notif.ts
@@ -2,7 +2,7 @@ import { AxiosRequestConfig } from 'axios';
 
 import request from "../utils/request";
 
-export async function notif(url: string, _:unknown, user_id: string) {
+export async function notif(url: string, token:unknown, user_id: string) {
   const cond = [['user_id', '=', user_id]];
   const fields = ['time', 'title', 'state', 'data'];
   const params = [
@@ -10,6 +10,10 @@ export async function notif(url: string, _:unknown, user_id: string) {
     'search_read_path',
     [cond, fields],
     { order: 'time desc', count: true, limit: 10, offset: 10 },
+    {
+      "user_id": user_id,
+      "token": token,
+    }
   ];
   
   const options:AxiosRequestConfig = {
