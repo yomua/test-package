8f1147f3d19ce62bb642fc9ff2d7e498857d8bd6
yanghongwei <>
2021-08-05 10:46:24 +0800

    modify

--- src/td.account/index.ts
+++ src/td.account/index.ts
@@ -1,4 +1,5 @@
 import request from '../utils/request';
+import { AxiosRequestConfig } from "axios";
 
 // options not require, should set default value in request.ts
 interface SDKConfig extends AxiosRequestConfig {
@@ -8,18 +9,16 @@ interface SDKConfig extends AxiosRequestConfig {
   company_id?: string;
   database?: string;
   usePublic?: boolean;
+  url?: string;
 }
 
-export const getCryptoAddress = async (
+ const getCryptoAddress = async (
   options?: SDKConfig,
   errorHandler?: ErrorHandler
 ) => {
-  const {
-    token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzMDI3NmUwMGQ4ODA3MjkxM2I0NTIiLCJkYiI6Im5mb19haXJjYXJib24iLCJpYXQiOjE2MjgwNTc5MTl9.h3aQwXhgNLHNzRpCYCIJum-kyAXMXAAFK0rYjCgb3MM',
-    user_id = 's0276e00d88ƒ072913b452',
-    url,
-  } = options;
+  const { token = '', user_id = '', url = '', } = options;
   const opt = {
+    method: 'POST',
     data: {
       id: Date.now(),
       method: 'execute',
@@ -38,3 +37,6 @@ export const getCryptoAddress = async (
   const data = await request(url, opt, errorHandler);
   return data;
 };
+
+
+export {getCryptoAddress}
